09b8e8798e1b5d06342ea8837c32a0f5112d814f
tomxi1997 <78306449+tomxi1997@users.noreply.github.com>
2023-11-18 21:45:44 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 ./build-llvm.py --vendor-string Pdx \
 --targets ARM AArch64 \
 --no-update \
--i /root/Toolchain/pdx-clang16
+-i /root/Toolchain/pdx-clang
 
 
 #./build-binutils.py --targets arm-linux-gnueabi aarch64-linux-gnu -i /root/Toolchain/pdx-clang16
